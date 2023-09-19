6190df85ab7320024a516d45d32bd36917fb36e6
NTIESPRN <140081943+NTIESPRN@users.noreply.github.com>
2023-09-19 10:11:46 -0300

    Update views.py

--- views.py
+++ views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 
+
 import re
 
 def format_data_nascimento(data):
