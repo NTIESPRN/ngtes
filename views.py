000dc843ccf59f9ceeaba07c95c540944caff12d
NTIESPRN <140081943+NTIESPRN@users.noreply.github.com>
2023-09-19 10:23:30 -0300

    Update views.py

--- views.py
+++ views.py
@@ -330,6 +330,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib import colors
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
 from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.units import inch
 from io import BytesIO
 from .models import Declaracao
 from .forms import DeclaracaoForm
